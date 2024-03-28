4d1d4e49a21493ba8cc0947cb159892d3b7de563
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-03-28 08:55:47 +0530

    2

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,10 +1,16 @@
+import { useUserContext } from '@/context/AuthContext'
 import BottomBar from '@/shared/BottomBar'
 import LeftSideBar from '@/shared/LeftSideBar'
 import TopBar from '@/shared/TopBar'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 function RootLayout() {
+  let navigate = useNavigate()
+  const { isAuthenticated } = useUserContext();
+  if(!isAuthenticated){
+    navigate('/sign-in')
+  }
   return (
     <div className='w-full md:flex'>
       <TopBar/>
