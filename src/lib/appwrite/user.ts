d264453d4dd8012e0896eaffb88990dd8c0b51f8
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-03-28 17:39:25 +0530

    quality

--- src/lib/appwrite/user.ts
+++ src/lib/appwrite/user.ts
@@ -177,7 +177,7 @@ export class Auth  {
           try {
             const fileUrl =  this.storage.getFilePreview(conf.BUCKET_ID,
               fileId,
-              2000,2000,'top',10
+              2000,2000,'top',50
               )
               return fileUrl
           } catch (error) {
